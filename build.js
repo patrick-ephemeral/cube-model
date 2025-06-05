10488863a3242bd55c44dd8306f105fb2def727b
Patrick <patrick@ephemeral-software.com>
2025-06-05 17:07:47 -0400

    Module and Example

--- build.js
+++ build.js
@@ -3,8 +3,9 @@ const esBuild = require('esbuild');
 (async () => {
     await esBuild.build({
         entryPoints: ['src/main.ts'],
-        outfile: 'dist/index.js',
+        outfile: 'dist/index.mjs',
         bundle: true,
         minify: true,
+        format: "esm",
     });
 })();
---
+++ sample.mjs
@@ -0,0 +1,42 @@
+import {
+    cubieCubeToStickerCube,
+    SOLVED_CUBIE_CUBE,
+} from "./dist/index.mjs";
+
+const stickers = cubieCubeToStickerCube(SOLVED_CUBIE_CUBE);
+
+/**
+ *                      C  C  C
+ *                   C  C  C            
+ *                C  C  C       D    E  E  E
+ *                           D  D    E  E  E
+ *  A  A  A    B  B  B    D  D  D    E  E  E
+ *  A  A  A    B  B  B    D  D
+ *  A  A  A    B  B  B    D
+ * 
+ *             F  F  F
+ *             F  F  F
+ *             F  F  F
+ */
+const printStickerCube = (sc) => {
+
+
+    console.log(`                     ${sc.U.topRow.leftSticker}  ${sc.U.topRow.centerSticker}  ${sc.U.topRow.rightSticker}`);
+    console.log(`                  ${sc.U.midRow.leftSticker}  ${sc.U.midRow.centerSticker}  ${sc.U.midRow.rightSticker}`);
+    console.log(`               ${sc.U.btmRow.leftSticker}  ${sc.U.btmRow.leftSticker}  ${sc.U.btmRow.rightSticker}       ${sc.R.topRow.rightSticker}    ${sc.B.topRow.leftSticker}  ${sc.B.topRow.centerSticker}  ${sc.B.topRow.rightSticker}`);
+    console.log(`                          ${sc.R.topRow.centerSticker}  ${sc.R.midRow.rightSticker}    ${sc.B.midRow.leftSticker}  ${sc.B.midRow.centerSticker}  ${sc.B.midRow.rightSticker}`);
+    console.log(` ${sc.L.topRow.leftSticker}  ${sc.L.topRow.centerSticker}  ${sc.L.topRow.rightSticker}    ${sc.F.topRow.leftSticker}  ${sc.F.topRow.centerSticker}  ${sc.F.topRow.rightSticker}    ${sc.R.topRow.leftSticker}  ${sc.R.midRow.centerSticker}  ${sc.R.btmRow.rightSticker}    ${sc.B.btmRow.leftSticker}  ${sc.B.btmRow.centerSticker}  ${sc.B.btmRow.rightSticker}`);
+    console.log(` ${sc.L.midRow.leftSticker}  ${sc.L.midRow.centerSticker}  ${sc.L.midRow.rightSticker}    ${sc.F.midRow.leftSticker}  ${sc.F.midRow.centerSticker}  ${sc.F.midRow.rightSticker}    ${sc.R.midRow.leftSticker}  ${sc.R.btmRow.centerSticker}`);
+    console.log(` ${sc.L.btmRow.leftSticker}  ${sc.L.btmRow.centerSticker}  ${sc.L.btmRow.rightSticker}    ${sc.F.btmRow.rightSticker}  ${sc.F.btmRow.rightSticker}  ${sc.F.btmRow.rightSticker}    ${sc.R.btmRow.leftSticker}`);
+    console.log(``);
+    console.log(`            ${sc.D.topRow.leftSticker}  ${sc.D.topRow.centerSticker}  ${sc.D.topRow.rightSticker}`);
+    console.log(`            ${sc.D.midRow.leftSticker}  ${sc.D.midRow.centerSticker}  ${sc.D.midRow.rightSticker}`);
+    console.log(`            ${sc.D.btmRow.leftSticker}  ${sc.D.btmRow.centerSticker}  ${sc.D.btmRow.rightSticker}`);
+
+
+
+};
+
+console.log();
+printStickerCube(stickers);
+console.log();
